53e2a65cca1d5e1cf22ef8a8a5fc7a35624044b8
Andradinho <raphaelandrade2013@gmail.com>
2021-01-24 11:47:04 -0300

    Implementing the keyboard.

--- calc/view/Keyboard.java
+++ calc/view/Keyboard.java
@@ -26,26 +26,29 @@ public class Keyboard extends JPanel {
 		addButton("%", COLOR_DARK_GRAY, c, 2, 0);
 		addButton("/", COLOR_ORANGE, c, 3, 0);
 		
+		// 2nd line
+		addButton("7", COLOR_LIGHT_GRAY, c, 0, 1);
+		addButton("8", COLOR_LIGHT_GRAY, c, 1, 1);
+		addButton("9", COLOR_LIGHT_GRAY, c, 2, 1);
+		addButton("*", COLOR_ORANGE, c, 3, 1);
+
+		// 3rd line
+		addButton("4", COLOR_LIGHT_GRAY, c, 0, 2);
+		addButton("5", COLOR_LIGHT_GRAY, c, 1, 2);
+		addButton("6", COLOR_LIGHT_GRAY, c, 2, 2);
+		addButton("-", COLOR_ORANGE, c, 3, 2);
 		
-		add(new Button("7", COLOR_LIGHT_GRAY));
-		add(new Button("8", COLOR_LIGHT_GRAY));
-		add(new Button("9", COLOR_LIGHT_GRAY));
-		add(new Button("*", COLOR_ORANGE));
+		// 4th line
+		addButton("1", COLOR_LIGHT_GRAY, c, 0, 3);
+		addButton("2", COLOR_LIGHT_GRAY, c, 1, 3);
+		addButton("3", COLOR_LIGHT_GRAY, c, 2, 3);
+		addButton("+", COLOR_ORANGE, c, 3, 3);
 		
-		add(new Button("7", COLOR_LIGHT_GRAY));
-		add(new Button("8", COLOR_LIGHT_GRAY));
-		add(new Button("9", COLOR_LIGHT_GRAY));
-		add(new Button("*", COLOR_ORANGE));
-		
-		add(new Button("7", COLOR_LIGHT_GRAY));
-		add(new Button("8", COLOR_LIGHT_GRAY));
-		add(new Button("9", COLOR_LIGHT_GRAY));
-		add(new Button("*", COLOR_ORANGE));
-		
-		add(new Button("7", COLOR_LIGHT_GRAY));
-		add(new Button("8", COLOR_LIGHT_GRAY));
-		add(new Button("9", COLOR_LIGHT_GRAY));
-		add(new Button("*", COLOR_ORANGE));
+		// 5th line
+		addButton("0", COLOR_LIGHT_GRAY, c, 0, 4);
+		addButton("0", COLOR_LIGHT_GRAY, c, 1, 4);
+		addButton(",", COLOR_LIGHT_GRAY, c, 2, 4);
+		addButton("=", COLOR_ORANGE, c, 3, 4);
 	}
 
 	private void addButton(String text, Color color, GridBagConstraints c, int x, int y) {
